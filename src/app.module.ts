ef6f623df4ecfe0bc2db8a6f9e2ea5650b04bd90
Juan Lee <ninejuan@protonmail.com>
2024-12-04 10:22:52 +0900

    add: auth, user res

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    DatabaseModule,
-  ],
+  imports: [AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+const env = process.env;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: [],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'Set-Cookie'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('PeQ API')
+    .setDescription('PeQ API')
+    .setVersion('1.0')
+    .addTag('peq')
+    .addCookieAuth('sessionId')
+    .build();
+  
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => {console.log("PeQ API Application Started")})
+  .catch((e) => {console.log("Failed to start PeQ API\n", e)});
