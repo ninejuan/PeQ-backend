35afb3d4acffc53e9d684eff1e0b110c209ac350
Juan Lee <ninejuan@protonmail.com>
2024-12-04 11:01:55 +0900

    add: auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import User from 'src/interface/user.interface';
+import userSchema from 'src/models/user.schema';
+import { JwtPayload } from 'src/interface/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  private readonly logger = new Logger(AuthService.name);
+
+  constructor(private jwtService: JwtService) {}
+
+  async handleGoogleLogin(user: any) {
+    const { email, firstName, lastName, picture, googleId } = user;
+
+    let existingUser: User = await this.findUserByEmail(email);
+
+    const generatedTokens = await this.generateTokens(existingUser);
+    return generatedTokens;
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async generateTokens(user: User) {
+    const payload: JwtPayload = {
+      google_id: user.google_uid,
+      google_mail: user.google_mail,
+    };
+
+    const accessToken = this.jwtService.sign(payload, { expiresIn: '1d' });
+    const refreshToken = this.jwtService.sign(payload, { expiresIn: '7d' });
+
+    await this.saveRefreshToken(user.google_mail, refreshToken);
+
+    return { accessToken: accessToken, refreshToken: refreshToken };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async refreshAccessToken(refreshToken: string) {
+    try {
+      const decoded = this.jwtService.verify(refreshToken);
+      const user = await this.findUserByEmail(decoded.email);
+
+      if (!user) {
+        throw new UnauthorizedException('Invalid token');
+      }
+
+      return this.generateTokens(user);
+    } catch (error) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+  async findUserByEmail(email: String) {
+    const user = await userSchema.findOne({
+      google_mail: `ninejuany@gmail.com`,
+    });
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async createUser(userDto: User): Promise<User> {
+    const newUser = await new userSchema(userDto).save();
+    return newUser;
   }
 }
--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -3,7 +3,7 @@ import mongo from 'mongoose';
 const userSchema = new mongo.Schema({
   google_mail: { type: String, required: true },
   name: { type: String, required: true },
-  description: { type: String, default: ' ' },
+  description: { type: String, default: '' },
   profilePhoto: { type: String, default: 'default.png' },
 });
 
