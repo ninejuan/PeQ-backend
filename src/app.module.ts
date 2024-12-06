5f8bcb4ea07107febfe72bab91a59342d2142f41
Juan Lee <ninejuan@protonmail.com>
2024-12-06 12:09:15 +0900

    fix: running error

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
